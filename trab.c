884d7eb27f0627884f172d80df234803f91b45b8
Tales Moehlecke <talesmoehlecke@gmail.com>
2019-09-09 21:50:02 -0300

    first commit

---
+++ trab.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdbool.h>
+/*
+int contador;
+
+void load_firefox(char *url[]){
+	//int status;
+        pid_t p = fork();                                       
+        if (p < 0) {                                            
+                fprintf(stderr, "Falha na criação do novo processo! \n");
+                exit(13);                                       
+        } else if (p == 0) {                                    
+                printf("Filho iniciando %s... \n", url[contador]); 
+                execlp("firefox","firefox --new-window", url[contador], NULL);                  
+                printf("Filho com parada de execução forçada! \n"); 
+                exit(14);
+        } else {       
+                printf("Pai esperando... \n"); 
+		//wait(&status);
+		sleep(5);
+		if(--contador>0) load_firefox(url);
+                printf("Pai finalizando... \n"); 
+                exit(0);
+        }
+}
+
+void start_browser(char *url[]){
+        load_firefox(url);
+}
+ */
+
+
+
+int main(int argc, char *argv[]){
+		printf("<<<< Applications Menu >>>\n1) Web Browser \t (executando, pid=1234) \n2) Text Editor \t(executando, pid=3456)\n3)Termina\t(falhou)\n4) Finalizar processo \t(concluído)\n5) Quit\n Opção: ");
+		bool whileTaRolando = true;
+		int entradaDoUsuario;
+		//pid_t p = fork();
+		while (whileTaRolando){
+			scanf("%d", &entradaDoUsuario);
+			//sleep(1);	
+			printf("%d", entradaDoUsuario);
+			printf("\n");
+			
+			switch(entradaDoUsuario){
+			case(1) :
+				printf("Abre Firefox");
+				break;
+			case(2) :
+				printf("Abre texto");
+
+				pid_t p = fork();                                       
+        			if (p < 0) {                                            
+                			fprintf(stderr, "Falha na criação do novo processo! \n");
+                			exit(13);                                       
+        			} 
+				else if (p == 0) {                                    
+                			printf("Filho iniciando... \n"); 
+					execlp("gedit", "gedit", NULL);	                
+					printf("Filho com parada de execução forçada! \n"); 
+					exit(14);
+				}
+
+
+
+
+
+		
+				break;
+			case(3) :
+				printf("Abre terminal");
+				break; 	
+			case(4) :
+				printf("Finalizar processo");
+				break; 	
+			default :
+				whileTaRolando = false;
+				break; 	
+			}
+		}
+	
+return 0;
+}
+
